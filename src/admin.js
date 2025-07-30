6a0d4acca30d4a789146bebc9937bbcc28c6de83
mkatbalasd <mkatbalasd@gmail.com>
2025-07-30 10:08:52 +0300

    Move express-ejs-layouts after view engine setup

--- src/admin.js
+++ src/admin.js
@@ -23,12 +23,11 @@ const expressLayouts = require('express-ejs-layouts');
 
 const app = express();
 expressWs(app);
-app.use(expressLayouts);
-app.set('layout', 'layout');
-
 const wsClients = new Set();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../views'));
+app.use(expressLayouts);
+app.set('layout', 'layout');
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 app.use('/static', express.static(path.join(__dirname, '../public')));
